a4547478ebd8f0408105876246bcf5fe0ae3ffc9
viktor-77 <vumanskiy0664041341@gmail.com>
2024-09-13 14:52:23 +0300

    Resolved Taxi service user form and validation

--- taxi/urls.py
+++ taxi/urls.py
@@ -46,9 +46,14 @@ urlpatterns = [
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
+    path(
+        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+    ),
     path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
-    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
+    path(
+        "drivers/<int:pk>/delete/", DriverDeleteView.as_view(),
+        name="driver-delete"
+    ),
     path(
         "drivers/<int:pk>/licence/update/",
         DriverLicenceUpdateView.as_view(),
